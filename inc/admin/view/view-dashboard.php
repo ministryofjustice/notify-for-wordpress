afa0ebda94b21f0423e6164b1013cfc434171785
Adam <adam.brown@digital.justice.gov.uk>
2018-10-11 08:33:13 +0100

    Text changes

--- inc/admin/view/view-dashboard.php
+++ inc/admin/view/view-dashboard.php
@@ -11,8 +11,11 @@ $outdated_pages = new Admin\Model();
 $context        = Agency_Context::get_agency_context();
 
 echo '<div class="wrap">';
+
 echo '<h2>' . _e( '<h1>Notify dashboard</h1>', $this->plugin_text_domain ) . '</h2>';
-echo '<p>Track all <strong>' . strtoupper( $context ) . '</strong> pages that have not been updated for over a year. Only includes pages, not events, news or blogs. If you want to track another agency\'s content use the admin agency switcher in the top right corner of this screen.<br /><br />Once you have updated the page, it will disappear from this list. Hover over the table titles to sort them by column.</p>';
+
+echo '<p>Track all <strong>' . strtoupper( $context ) . '</strong> pages that have not been updated for over a year. This includes pages but not events, news or blogs. If you want to track another agency\'s content, use the admin agency switcher in the top right corner of this screen.<br /><br />Once the page listed below has been updated (by saving the page), it will disappear from this list. You can sort by column by hovering over the table titles.</p>';
+
 echo '<h3>Pages</h3>';
 echo '<div id="nfwp-table">';
 echo '<div id="nfwp-table-post-body">';
