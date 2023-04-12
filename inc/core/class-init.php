1f1ad39155804531e87506b4273a90058bf31175
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-04-12 10:56:24 +0100

    Fix deprecations and update README.md

--- inc/core/class-init.php
+++ inc/core/class-init.php
@@ -2,150 +2,139 @@
 
 namespace Notify_For_Wordpress\Inc\Core;
 
-use Notify_For_Wordpress as NF;
+use Notify_For_Wordpress;
 use Notify_For_Wordpress\Inc\Admin as Admin;
 
 /**
-* class-init.php is the core file responsible for including and
-* instantiating much of the code that composes the plugin
-*
-* @package NFWP
-*
-* @since 0.2.0
-*/
+ * class-init.php is the core file responsible for including and
+ * instantiating much of the code that composes the plugin
+ *
+ * @package NFWP
+ *
+ * @since 0.2.0
+ */
 
 // Exit if file is accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	die();
+if (!defined('ABSPATH')) {
+    die();
 }
 
-class Init {
+class Init
+{
 
-	/**
-	 * A reference to the loader class that coordinates the hooks and callbacks
-	 * throughout the plugin.
-	 *
-	 * @var    class $loader Manages hooks between the WordPress hooks and the callback functions.
-	 */
-	protected $loader;
+    /**
+     * A reference to the loader class that coordinates the hooks and callbacks
+     * throughout the plugin.
+     *
+     * @var    class $loader Manages hooks between the WordPress hooks and the callback functions.
+     */
+    protected $loader;
 
-	/**
-	 * Represents the slug of the plugin that can be used throughout the plugin
-	 * for internationalization and other purposes.
-	 *
-	 * @var    string $plugin_name The single, hyphenated string used to identify this plugin.
-	 */
-	protected $plugin_name;
+    /**
+     * Maintains the current version of the plugin so that we can use it throughout
+     * the plugin.
+     *
+     * @var    string $plugin_basename The current version of the plugin.
+     */
+    protected $plugin_basename;
 
-	/**
-	 * Maintains the current version of the plugin so that we can use it throughout
-	 * the plugin.
-	 *
-	 * @var    string $version The current version of the plugin.
-	 */
-	protected $version;
+    /**
+     * Instantiates the plugin by setting up the core properties and loading
+     * all necessary dependencies and defining the hooks.
+     */
+    public function __construct()
+    {
+        $this->plugin_basename = Notify_For_Wordpress\PLUGIN_BASENAME;
 
-	/**
-	 * Instantiates the plugin by setting up the core properties and loading
-	 * all necessary dependencies and defining the hooks.
-	 */
-	public function __construct() {
+        $this->load_dependencies();
+        $this->define_admin_hooks();
+    }
 
-		$this->plugin_name 				= NF\PLUGIN_NAME;
-		$this->version            = NF\PLUGIN_VERSION;
-		$this->plugin_basename    = NF\PLUGIN_BASENAME;
-		$this->plugin_text_domain = NF\PLUGIN_TEXT_DOMAIN;
+    /**
+     * Loads all the plugin's main action hooks calling various functions.
+     */
+    private function load_dependencies()
+    {
+        $this->loader = new Loader();
+    }
 
-		$this->load_dependencies();
-		$this->define_admin_hooks();
-		//$this->define_metabox_hooks();
-		//$this->define_email_hooks();
-	}
+    /**
+     * Defines the hooks and callback functions that are used for setting up the plugin stylesheets
+     * and the plugin's meta box.
+     *
+     * This function relies on the Notify For WordPress Admin class and the Notify For WordPress
+     * Loader class property.
+     */
+    private function define_admin_hooks()
+    {
+        $plugin_admin = new Admin\Admin($this->get_plugin_name(), $this->get_version(), $this->get_plugin_text_domain());
 
-	/**
-	 * Loads all the plugin's main action hooks calling various functions.
-	 */
-	private function load_dependencies() {
-		$this->loader = new Loader();
-	}
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+        $this->loader->add_action('admin_menu', $plugin_admin, 'notify_for_wordpress_menu');
+    }
 
-	/**
-	 * Defines the hooks and callback functions that are used for setting up the plugin stylesheets
-	 * and the plugin's meta box.
-	 *
-	 * This function relies on the Notify For WordPress Admin class and the Notify For WordPress
-	 * Loader class property.
-	 */
-	private function define_admin_hooks() {
+    /**
+    * This function relies on the Notify For WordPress Metabox class and the Notify For WordPress
+    * Loader class property.
+    */
+    private function define_metabox_hooks()
+    {
+        $plugin_admin = new Admin\Metabox($this->get_version());
 
-		$plugin_admin = new Admin\Admin( $this->get_plugin_name(), $this->get_version(), $this->get_plugin_text_domain() );
+        $this->loader->add_action('init', $plugin_admin, 'init_metabox');
+    }
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_menu', $plugin_admin, 'notify_for_wordpress_menu' );
-	}
+    /**
+    * This function relies on the Notify For WordPress Email class and the Notify For WordPress
+    * Loader class property.
+    */
+    private function define_email_hooks()
+    {
 
-	/*
-	*
-	* This function relies on the Notify For WordPress Metabox class and the Notify For WordPress
-	* Loader class property.
-	*
-	*/
-	private function define_metabox_hooks() {
+        $plugin_admin = new Admin\Email($this->get_version());
 
-		$plugin_admin = new Admin\Metabox( $this->get_version() );
+        $this->loader->add_action('init', $plugin_admin, 'init_email');
+    }
 
-		$this->loader->add_action('init', $plugin_admin, 'init_metabox');
+    /**
+     * Sets this class into motion.
+     *
+     * Executes the plugin by calling the run method of the loader class which will
+     * register all the hooks and callback functions used throughout the plugin
+     * with WordPress.
+     */
+    public function run()
+    {
+        $this->loader->run();
+    }
 
-	}
+    /**
+     * The name of the plugin used to uniquely identify it within the context of
+     * WordPress and to define internationalization functionality.
+     */
+    public function get_plugin_name()
+    {
+        return Notify_For_Wordpress\PLUGIN_NAME;
+    }
 
-	/*
-	*
-	* This function relies on the Notify For WordPress Email class and the Notify For WordPress
-	* Loader class property.
-	*
-	*/
-	private function define_email_hooks() {
+    /**
+     * Returns the current version of the plugin to the caller.
+     *
+     * @return string $this->version The current version of the plugin.
+     */
+    public function get_version()
+    {
+        return Notify_For_Wordpress\PLUGIN_VERSION;
+    }
 
-		$plugin_admin = new Admin\Email( $this->get_version() );
-
-		$this->loader->add_action('init', $plugin_admin, 'init_email');
-	}
-
-	/**
-	 * Sets this class into motion.
-	 *
-	 * Executes the plugin by calling the run method of the loader class which will
-	 * register all of the hooks and callback functions used throughout the plugin
-	 * with WordPress.
-	 */
-	public function run() {
-		$this->loader->run();
-	}
-
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 */
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
-
-	/**
-	 * Returns the current version of the plugin to the caller.
-	 *
-	 * @return string $this->version The current version of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
-
-	/**
-	 * Retrieve the text domain of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The text domain of the plugin.
-	 */
-	public function get_plugin_text_domain() {
-		return $this->plugin_text_domain;
-	}
+    /**
+     * Retrieve the text domain of the plugin.
+     *
+     * @return    string    The text domain of the plugin.
+     * @since     1.0.0
+     */
+    public function get_plugin_text_domain()
+    {
+        return Notify_For_Wordpress\PLUGIN_TEXT_DOMAIN;
+    }
 }
--- notify-for-wordpress.php
+++ notify-for-wordpress.php
@@ -2,7 +2,7 @@
 /**
  * The file responsible for starting the Notify for WordPress plugin
  *
- * The Notify for WordPress is a plugin that allows users to setup notifications
+ * Notify for WordPress is a plugin that allows users to setup notifications
  * on out of date content.
  *
  * @package NFWP
@@ -11,7 +11,7 @@
  * Plugin Name:       Notify for WordPress
  * Plugin URI:        http://wordpress.org/extend/plugins/
  * Description:       A notification plugin to help editors keep their content up-to-date
- * Version:           0.1.0
+ * Version:           0.2.1
  * Text Domain:       notify-for-wordpress
  * Domain Path:       /languages
  * Author:            Ministry of Justice
@@ -20,49 +20,29 @@
  * Copyright:         Crown Copyright (c) 2018 Ministry of Justice
  */
 
- /*
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-
-*/
 namespace Notify_For_Wordpress;
 
 // Exit if file is accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	die();
+if (!defined('ABSPATH')) {
+    die();
 }
 
 /**
  * Define Constants
  */
-define( __NAMESPACE__ . '\NF', __NAMESPACE__ . '\\' );
+const NF = __NAMESPACE__ . '\\';
 
-define( NF . 'PLUGIN_NAME', 'notify-for-wordpress' );
+define(NF . 'PLUGIN_NAME', 'notify-for-wordpress');
 
-define( NF . 'PLUGIN_VERSION', '0.2.0' );
+define(NF . 'PLUGIN_VERSION', '0.2.1');
 
-define( NF . 'PLUGIN_NAME_DIR', plugin_dir_path( __FILE__ ) );
+define(NF . 'PLUGIN_NAME_DIR', plugin_dir_path(__FILE__));
 
-define( NF . 'PLUGIN_NAME_URL', plugin_dir_url( __FILE__ ) );
+define(NF . 'PLUGIN_NAME_URL', plugin_dir_url(__FILE__));
 
-define( NF . 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define(NF . 'PLUGIN_BASENAME', plugin_basename(__FILE__));
 
-define( NF . 'PLUGIN_TEXT_DOMAIN', 'notify-for-wordpress' );
+define(NF . 'PLUGIN_TEXT_DOMAIN', 'notify-for-wordpress');
 
 /**
  * Autoload Classes
@@ -73,13 +53,13 @@ require_once PLUGIN_NAME_DIR . 'inc/libraries/autoloader.php';
  * Register Activation and Deactivation Hooks
  * This action is documented in inc/core/class-activator.php
  */
-register_activation_hook( __FILE__, array( NF . 'Inc\Core\Activator', 'activate' ) );
+register_activation_hook(__FILE__, array(NF . 'Inc\Core\Activator', 'activate'));
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented inc/core/class-deactivator.php
  */
-register_deactivation_hook( __FILE__, array( NF . 'Inc\Core\Deactivator', 'deactivate' ) );
+register_deactivation_hook(__FILE__, array(NF . 'Inc\Core\Deactivator', 'deactivate'));
 
 /**
  * Plugin Singleton Container
@@ -91,22 +71,24 @@ register_deactivation_hook( __FILE__, array( NF . 'Inc\Core\Deactivator', 'deact
 class Notify_For_Wordpress
 {
 
-	static $init;
-	/**
-	 * Loads the plugin
-	 *
-	 * @access    public
-	 */
-	public static function init() {
+    static $init;
 
-		if ( null == self::$init ) {
+    /**
+     * Loads the plugin
+     *
+     * @access    public
+     */
+    public static function init()
+    {
 
-			self::$init = new Inc\Core\Init();
-			self::$init->run();
-		}
+        if (null == self::$init) {
 
-		return self::$init;
-	}
+            self::$init = new Inc\Core\Init();
+            self::$init->run();
+        }
+
+        return self::$init;
+    }
 
 }
 
@@ -121,13 +103,14 @@ class Notify_For_Wordpress
  * can interact with the plugin's hooks contained within.
  *
  */
-function notify_for_wordpress_init() {
-		return Notify_For_Wordpress::init();
+function notify_for_wordpress_init()
+{
+    return Notify_For_Wordpress::init();
 }
 
 $min_php = '5.6.0';
 
 // Check the minimum required PHP version and run the plugin.
-if ( version_compare( PHP_VERSION, $min_php, '>=' ) ) {
-		notify_for_wordpress_init();
+if (version_compare(PHP_VERSION, $min_php, '>=')) {
+    notify_for_wordpress_init();
 }
